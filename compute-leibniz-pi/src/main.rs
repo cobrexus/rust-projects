2bc985a1b2d6811c5e93f66d47022439b8479daa
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2024-04-16 19:31:43 +0100

    Add compute-leibniz-pi

---
+++ compute-leibniz-pi/src/main.rs
@@ -0,0 +1,26 @@
+use std::time::Instant;
+
+fn main() {
+    let now = Instant::now();
+
+    let mut pi = 0.0;
+    let mut pos = true;
+
+    for i in (1..10000000000_i64).step_by(2) {
+        match pos {
+            true => {
+                pi += 1.0 / (i as f64);
+            }
+            false => {
+                pi -= 1.0 / (i as f64);
+            }
+        };
+
+        pos = !pos;
+    }
+
+    pi *= 4.0;
+
+    let elapsed = now.elapsed();
+    println!("{} in {:.2?}", pi, elapsed);
+}

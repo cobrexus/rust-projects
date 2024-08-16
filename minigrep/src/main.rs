eb926895db410fef50d6e3ee1169f8e0e6ce8fdf
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2024-08-16 22:26:24 +0100

    Refactor

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,15 +4,13 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|e| {
-        eprintln!("{e}");
+    let config = Config::build(env::args()).unwrap_or_else(|e| {
+        eprintln!("error: {e}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        eprintln!("{e}");
+        eprintln!("error: {e}");
         process::exit(1);
     }
 }
